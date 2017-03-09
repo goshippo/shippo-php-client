176d7f02c2ab1918f1dcd30267dce037d94651ba
Guozhen La <guozhenla@guozhen-shippo-mbp.local>
2017-03-09 10:28:30 -0800

    remove rate list

--- lib/Shippo/Rate.php
+++ lib/Shippo/Rate.php
@@ -12,15 +12,4 @@ class Shippo_Rate extends Shippo_ApiResource
         $class = get_class();
         return self::_scopedRetrieve($class, $id, $apiKey);
     }
-    
-    /**
-     * @param array|null $params
-     *
-     * @return Shippo_All Get all the Rates.
-     */
-    public static function all($params = null, $apiKey = null)
-    {
-        $class = get_class();
-        return self::_scopedAll($class, $params, $apiKey);
-    }
 }
